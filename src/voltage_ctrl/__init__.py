fb2aae2fb75e040ab67fe6493b8a4bc9b7424775
Kok-Wei Bong <bongkokwei@gmail.com>
2025-12-11 16:12:25 +1100

    edit

--- src/voltage_ctrl/__init__.py
+++ src/voltage_ctrl/__init__.py
@@ -8,8 +8,6 @@ communication with Arduino-based DAC systems.
 from .power_supply_controller import PowerSupplyController
 
 __version__ = "0.1.0"
-__author__ = "Your Name"
-__all__ = ["PowerSupplyController"]
 
 # Package-level constants
 DAC_RESOLUTION = 4096  # 12-bit DAC
